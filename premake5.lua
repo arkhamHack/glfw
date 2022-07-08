f2281a6b048513111def078abd7ce8cfde60355c
Avigyan Sinha <72064090+arkhamHack@users.noreply.github.com>
2022-07-09 01:47:36 +0530

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -4,8 +4,8 @@ project "GLFW"
     language "C"
     staticruntime "off"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    targetdir ("bin/" .. outputDir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputDir .. "/%{prj.name}")
 
     files{
         "include/GLFW/glfw3.h",
